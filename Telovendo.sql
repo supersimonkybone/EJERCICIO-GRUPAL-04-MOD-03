256092a8e80e5348accab989c7b27ef219609aa8
Simon Zelada <128656840+supersimonkybone@users.noreply.github.com>
2023-04-18 22:47:29 -0400

    Update Telovendo.sql

--- Telovendo.sql
+++ Telovendo.sql
@@ -1,1 +1,12 @@
-
+-- CREACION DE TABLA Y ENLACE A LA TABLA CLIENTES
+CREATE TABLE Cuentas (
+  codigo INT NOT NULL,
+  saldo BIGINT DEFAULT 0,
+  PRIMARY KEY (codigo),
+  FOREIGN KEY (codigo) REFERENCES cliente(id)
+);
+-- TRANSACCION PARA CADA TRANSFERENCIA CON VALIDACION
+START TRANSACTION;
+UPDATE Cuentas SET saldo = saldo - cantidad_a_transferir WHERE codigo = codigo_origen AND saldo >= cantidad_a_transferir;
+UPDATE Cuentas SET saldo = saldo + cantidad_a_transferir WHERE codigo = codigo_destino;
+COMMIT;
