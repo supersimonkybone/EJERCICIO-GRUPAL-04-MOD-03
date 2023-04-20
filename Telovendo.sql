590d17f30ae9275bb893b71ce74d544bc140ff2f
Simon Zelada <128656840+supersimonkybone@users.noreply.github.com>
2023-04-19 21:42:02 -0400

    Update Telovendo.sql

--- Telovendo.sql
+++ Telovendo.sql
@@ -6,11 +6,21 @@ CREATE TABLE Cuentas (
   FOREIGN KEY (codigo) REFERENCES cliente(id)
 );
 -- TRANSACCION PARA CADA TRANSFERENCIA CON VALIDACION
+
+DECLARE transferencia INT DEFAULT 0;
+DECLARE codigo_origen  ;
+DECLARE codigo_destino  ;
+
+SET transferencia = 100;
+SET codigo_origen = 001;
+SET codigo_destino = 002;
+
 START TRANSACTION;
-IF saldo >= cantidad_a_transferir THEN
-UPDATE Cuentas SET saldo = saldo - cantidad_a_transferir WHERE codigo = codigo_origen AND saldo >= cantidad_a_transferir;
-UPDATE Cuentas SET saldo = saldo + cantidad_a_transferir WHERE codigo = codigo_destino;
+IF saldo >= transferencia THEN
+UPDATE Cuentas SET saldo = saldo - transferencia WHERE codigo = codigo_origen AND saldo >= transferencia;
+UPDATE Cuentas SET saldo = saldo + transferencia WHERE codigo = codigo_destino;
 ELSE
+    SELECT 'Alerta, no dispone de saldo suficiente';
     ROLLBACK;
 END IF;
 COMMIT;
